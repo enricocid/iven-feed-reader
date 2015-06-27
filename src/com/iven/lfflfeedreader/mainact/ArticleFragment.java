cdb7b5e17572ccbb9f62a92af4956d0749a14ac1
EnricoD <enrico2588@gmail.com>
2015-06-27 20:49:08 +0200

    implement browser chooser & webview history clear

--- src/com/iven/lfflfeedreader/mainact/ArticleFragment.java
+++ src/com/iven/lfflfeedreader/mainact/ArticleFragment.java
@@ -3,6 +3,8 @@ package com.iven.lfflfeedreader.mainact;
 import com.iven.lfflfeedreader.R;
 import com.iven.lfflfeedreader.domparser.RSSFeed;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.webkit.WebSettings.LayoutAlgorithm;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -36,18 +39,38 @@ public class ArticleFragment extends Fragment {
 
 		ScrollView sv = (ScrollView) view.findViewById(R.id.sv);
 		sv.setVerticalFadingEdgeEnabled(true);
-		sv.setPadding(15, 15, 15, 0);
 
 		// Set webview settings
 		WebSettings ws = wb.getSettings();
-		ws.setLayoutAlgorithm(LayoutAlgorithm.NORMAL);
+		ws.setLayoutAlgorithm(LayoutAlgorithm.TEXT_AUTOSIZING);
 		
 		// Set the views
 				
 		title.setText(fFeed.getItem(fPos).getTitle()) ;
 		
 		wb.loadData(fFeed.getItem(fPos).getDescription(), "text/html; charset=utf-8;", "UTF-8") ;
-		
+		wb.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            	Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            	CharSequence title2 = getResources().getText(R.string.chooser_title);
+                Intent chooser = Intent.createChooser(intent, title2);
+            	startActivity(chooser);
+				return true;
+            }
+        @Override
+        public void onLoadResource(WebView view, String url) {
+            // Notice Here.
+            view.clearHistory();
+            super.onLoadResource(view, url);
+        }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            // And Here.
+            view.clearHistory();
+            super.onPageFinished(view,url);
+        }
+});
 		return view;
 	}
 }
