5500aec74a018256d068af738039c6cc1d4454e8
EnricoD <enrico2588@gmail.com>
2015-07-30 12:05:05 +0200

    create utils folder

--- project/app/src/main/java/com/iven/lfflfeedreader/utils/fabscroll.java
+++ project/app/src/main/java/com/iven/lfflfeedreader/utils/fabscroll.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.iven.lfflfeedreader.mainact;
+package com.iven.lfflfeedreader.utils;
 
 import com.iven.lfflfeedreader.R;
 
