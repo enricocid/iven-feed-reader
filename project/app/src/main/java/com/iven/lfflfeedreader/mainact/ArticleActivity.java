45ecc75b0c067c34dfbbd4622ce7c7276a96147c
EnricoD <enrico2588@gmail.com>
2015-10-25 10:23:16 +0100

    implement "Immersive mode" & update styles/strings

--- project/app/src/main/java/com/iven/lfflfeedreader/mainact/ArticleActivity.java
+++ project/app/src/main/java/com/iven/lfflfeedreader/mainact/ArticleActivity.java
@@ -40,12 +40,22 @@ public class ArticleActivity extends AppCompatActivity {
 		pager.setAdapter(mPagerAdapter);
         pager.setCurrentItem(pos);
         pager.setClipToPadding(false);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
 		if (Preferences.navTintEnabled(this)) {
 			getWindow().setNavigationBarColor(getResources().getColor(R.color.lffl2));
 		}
 
-		final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (Preferences.immersiveEnabled(this)) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        }
 
 	    setSupportActionBar(toolbar);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
