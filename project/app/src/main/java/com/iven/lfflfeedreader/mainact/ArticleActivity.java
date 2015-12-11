e37d5059df4380e905d2b8e61e72f59cf26f29b2
EnricoD <ivenfeedreader@outlook.com>
2015-12-11 18:17:03 +0100

    clean unused imports

--- project/app/src/main/java/com/iven/lfflfeedreader/mainact/ArticleActivity.java
+++ project/app/src/main/java/com/iven/lfflfeedreader/mainact/ArticleActivity.java
@@ -12,10 +12,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-
 import com.iven.lfflfeedreader.R;
 import com.iven.lfflfeedreader.domparser.RSSFeed;
 import com.iven.lfflfeedreader.utils.Preferences;
