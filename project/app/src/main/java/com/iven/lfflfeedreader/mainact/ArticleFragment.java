97ff5312bc279f91ffda64d7e09e6cff907e106a
EnricoD <ivenfeedreader@outlook.com>
2015-12-28 13:54:15 +0100

    simplify ArticleFragment

--- project/app/src/main/java/com/iven/lfflfeedreader/mainact/ArticleFragment.java
+++ project/app/src/main/java/com/iven/lfflfeedreader/mainact/ArticleFragment.java
@@ -5,6 +5,7 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.iven.lfflfeedreader.R;
 import com.iven.lfflfeedreader.domparser.RSSFeed;
+import com.iven.lfflfeedreader.domparser.RSSItem;
 import com.iven.lfflfeedreader.utils.Preferences;
 
 import android.content.Intent;
@@ -30,7 +31,16 @@ public class ArticleFragment extends Fragment {
 
     //feed
 	RSSFeed fFeed;
-    String base2;
+    RSSItem feedItem;
+    String feedDescription;
+    String feedCompleteDescription;
+    String datDescription;
+    String imageLink;
+    String imageLink2;
+    String feedLink;
+    String feedTitle;
+    String feedDate;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 
@@ -39,7 +49,19 @@ public class ArticleFragment extends Fragment {
         //Initialize the feed (i.e. get all the data)
         fFeed = (RSSFeed) getArguments().getSerializable("feed");
 		fPos = getArguments().getInt("pos");
-	}
+        feedItem = fFeed.getItem(fPos);
+
+        //get the content
+        feedCompleteDescription = feedItem.getCompleteDescription();
+        feedDescription = feedItem.getDescription();
+        imageLink = feedItem.getImage();
+        imageLink2 = feedItem.getImage2();
+        feedLink = feedItem.getLink();
+        feedTitle = feedItem.getTitle();
+        feedDate = feedItem.getDate();
+        datDescription = feedItem.getDescription();
+
+    }
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -66,7 +88,7 @@ public class ArticleFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 if (Preferences.WebViewEnabled(getContext())) {
-                    openFeedPage(fFeed.getItem(fPos).getLink());
+                    openFeedPage(feedLink);
                 } else {
                     continueReading();
                 }
@@ -106,10 +128,10 @@ public class ArticleFragment extends Fragment {
 
         //dynamically set title and subtitle according to the feed data
         //set title of the article
-        title.setText(fFeed.getItem(fPos).getTitle());
+        title.setText(feedTitle);
 
         //set the date of the article to subtitle
-        subtitle.setText(fFeed.getItem(fPos).getDate());
+        subtitle.setText(feedDate);
 
         //set the articles text size from preferences
         //little explanation about setTextSize(TypedValue.COMPLEX_UNIT_SP, size);
@@ -139,10 +161,10 @@ public class ArticleFragment extends Fragment {
 
         //else, load the image
         //if getImage() method fails (i.e when img is in content:encoded) load image2
-        else if (fFeed.getItem(fPos).getImage().isEmpty()) {
+        else if (imageLink.isEmpty()) {
 
             //use glide to load the image into the imageview (imageView)
-            Glide.with(getActivity()).load(fFeed.getItem(fPos).getImage2())
+            Glide.with(getActivity()).load(imageLink2)
 
                     //disable cache to avoid garbage collection that may produce crashes
                     .diskCacheStrategy(DiskCacheStrategy.NONE)
@@ -152,7 +174,7 @@ public class ArticleFragment extends Fragment {
         } else {
 
             //load the parsed article's image using glide
-            Glide.with(getActivity()).load(fFeed.getItem(fPos).getImage())
+            Glide.with(getActivity()).load(imageLink)
 
                     //disable cache to avoid garbage collection that may produce crashes
                     .diskCacheStrategy(DiskCacheStrategy.NONE)
@@ -167,14 +189,15 @@ public class ArticleFragment extends Fragment {
 
                                                      //use glide to load the image into the imageview (imageView)
                                                      //if getImage() method fails (i.e when img is in content:encoded) load image2
-                                                     if (fFeed.getItem(fPos).getImage().isEmpty()) {
-                                                         intent = new Intent(Intent.ACTION_VIEW, Uri.parse(fFeed.getItem(fPos).getImage2()));
+                                                     if (imageLink.isEmpty()) {
+
+                                                         intent = new Intent(Intent.ACTION_VIEW, Uri.parse(imageLink2));
 
                                                      }
 
                                                      //else use image
                                                      else {
-                                                         intent = new Intent(Intent.ACTION_VIEW, Uri.parse(fFeed.getItem(fPos).getImage()));
+                                                         intent = new Intent(Intent.ACTION_VIEW, Uri.parse(imageLink));
 
                                                      }
                                                      CharSequence title2 = getResources().getText(R.string.chooser_title);
@@ -189,31 +212,31 @@ public class ArticleFragment extends Fragment {
 
         //we use complete description by default, but sometimes the method returns null
         //so, if getCompleteDescription is null use description
-        if(fFeed.getItem(fPos).getCompleteDescription().contains("no desc")) {
-            base2 = Jsoup.parse(fFeed.getItem(fPos).getDescription()).text().replace("Continua a leggere...", "");
+        if(feedCompleteDescription.contains("no desc")) {
+            datDescription = Jsoup.parse(feedDescription).text().replace("Continua a leggere...", "");
 
         // else, use complete description
         } else {
-            base2 = Jsoup.parse(fFeed.getItem(fPos).getCompleteDescription()).text().replace("Continua a leggere...", "");
+            datDescription = Jsoup.parse(feedCompleteDescription).text().replace("Continua a leggere...", "");
 
         }
 
         //replace some items since this is a simple textview
         //and continue reading hyperlinks are not clickable
         //so we are going to replace them with an empty text
-		String base2format = base2.replace("Continue reading...", "");
+		String datDescription2format = datDescription.replace("Continue reading...", "");
 
-        String base3format = base2format.replace("Visit on site http://www.noobslab.com", "");
+        String datDescription3format = datDescription2format.replace("Visit on site http://www.noobslab.com", "");
 
-        String base4format = base3format.replace("Read More", "");
+        String datDescription4format = datDescription3format.replace("Read More", "");
 
-        String base5format = base4format.replace("(read more)", "");
+        String datDescription5format = datDescription4format.replace("(read more)", "");
 
         //load the article inside a text view
         TextView articletext = (TextView) rootView.findViewById(R.id.webv);
 
         //set the articles text
-		articletext.setText(base5format);
+		articletext.setText(datDescription5format);
 
         //set the article text size according to preferences
         articletext.setTextSize(TypedValue.COMPLEX_UNIT_SP, size);
@@ -226,13 +249,13 @@ public class ArticleFragment extends Fragment {
     private void share() {
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
-        shareIntent.putExtra(Intent.EXTRA_TEXT, fFeed.getItem(fPos).getLink());
-        startActivity(Intent.createChooser(shareIntent, getString(R.string.share) + " '" + fFeed.getItem(fPos).getTitle() + "'"));
+        shareIntent.putExtra(Intent.EXTRA_TEXT, feedLink);
+        startActivity(Intent.createChooser(shareIntent, getString(R.string.share) + " '" + feedTitle + "'"));
     }
 
     //Read more method
     private void continueReading() {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(fFeed.getItem(fPos).getLink()));
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(feedLink));
         CharSequence title2 = getResources().getText(R.string.chooser_title);
         Intent chooser = Intent.createChooser(intent, title2);
         startActivity(chooser);
@@ -245,6 +268,7 @@ public class ArticleFragment extends Fragment {
         AppCompatActivity activity = (AppCompatActivity) getActivity();
         activity.onBackPressed();
     }
+
     //method to open the feed link using a webview
     private void openFeedPage(String datfeed)
     {
@@ -252,6 +276,7 @@ public class ArticleFragment extends Fragment {
         final Intent intent = new Intent(getActivity(), ArticlePage.class);
 
         intent.putExtra("feedselected", datfeed);
+
 
         //and start a new ArticlePage activity with the selected feed
         startActivity(intent);
